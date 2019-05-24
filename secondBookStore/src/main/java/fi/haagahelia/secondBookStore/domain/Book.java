1c4a30dcdf2d30d95f59e8166698f5355fe09dc1
ashleynguci <40316179+ashleynguci@users.noreply.github.com>
2019-05-24 17:32:09 +0300

    Create REST service that return all books (JSON)

--- secondBookStore/src/main/java/fi/haagahelia/secondBookStore/domain/Book.java
+++ secondBookStore/src/main/java/fi/haagahelia/secondBookStore/domain/Book.java
@@ -4,10 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Book {
@@ -18,22 +14,16 @@ public class Book {
 
 	private int year, isbn,price;
 
-	@ManyToOne
-	@JsonIgnore
-	@JoinColumn(name = "categoryid")
-	private Category category;
-
+	
 	public Book() {}
 	
-	public Book(String title, String author, int year, Category category, int isbn, int price) {
+	public Book(String title, String author, int year, int isbn, int price) {
 		super();
 		this.title = title;
 		this.author = author;
 		this.year = year;
 		this.isbn = isbn;
 		this.price = price;
-		this.category = category;
-		
 	}
 
 	public long getId() {
@@ -82,19 +72,6 @@ public class Book {
 
 	public void setPrice(int price) {
 		this.price = price;
-	}
-	public Category getCategory() {
-		return category;
-	}
-
-	public void setCategory(Category category) {
-		this.category = category;
-	}
-	
-	@Override
-	public String toString() {
-		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year + ", ISBN=" + isbn
-				+ ", price=" + price + "]";
 	}
 	
 }
--- secondBookStore/src/main/java/fi/haagahelia/secondBookStore/domain/BookRepository.java
+++ secondBookStore/src/main/java/fi/haagahelia/secondBookStore/domain/BookRepository.java
@@ -1,9 +1,7 @@
 package fi.haagahelia.secondBookStore.domain;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
-	List<Book> findByTitle(String title);
+
 }
