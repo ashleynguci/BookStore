9baa41f60cc7f8a91aeb9f694a9fc43908e4b957
ashleynguci <40316179+ashleynguci@users.noreply.github.com>
2019-06-02 22:12:14 +0300

    Create unit tests in Spring Boot ( JUnit)

--- secondBookStore/src/main/java/fi/haagahelia/secondBookStore/SecondBookStoreApplication.java
+++ secondBookStore/src/main/java/fi/haagahelia/secondBookStore/SecondBookStoreApplication.java
@@ -31,17 +31,20 @@ public class SecondBookStoreApplication {
 		crepository.save(new Category("Comic"));
 		crepository.save(new Category("Crime"));
 		crepository.save(new Category("Education"));
+		crepository.save(new Category("Romance novel"));
 		
 		Book b1 = new Book("If tomorrow comes","Sydney Sheldon", 1985,crepository.findByName("Crime").get(0),12223, 35.50);
 		Book b2 = new Book("The Lord of the Rings","J. R. R. Tolkien", 1937,crepository.findByName("Non-fiction").get(0),12224, 45.00);
 		Book b3 = new Book("Frankenstein","Mary Shelley", 1823,crepository.findByName("Education").get(0),12225, 25.50);
 		Book b4 = new Book("Hamlet"," William Shakespeare", 1609,crepository.findByName("Comic").get(0),12221, 55.00);
+		Book b5 = new Book("Dear John","Nicholas Sparks", 2006,crepository.findByName("Romance novel").get(0),12226, 25.50);
 
 		
 		repository.save(b1);
 		repository.save(b2);
 		repository.save(b3);
 		repository.save(b4);
+		repository.save(b5);
 		// Create users: admin/admin user/user
 		User user1 = new User("user", "$2a$10$fV9K5C3ukO6U70tUrhhAKuo2f4qNFrnbGwBL4m1PN4cysXLhpcFsG", "USER");
 		User user2 = new User("admin", "$2a$10$Qo7EVRUdmp1aWLs0aCyStevP4AcbcibExWKtg32IJwK0oJGnIA6Jy", "ADMIN");
---
+++ secondBookStore/src/test/java/fi/haagahelia/secondBookStore/BookRepositoryTest.java
@@ -0,0 +1,39 @@
+package fi.haagahelia.secondBookStore;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import fi.haagahelia.secondBookStore.domain.Category;
+import fi.haagahelia.secondBookStore.domain.Book;
+import fi.haagahelia.secondBookStore.domain.BookRepository;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class BookRepositoryTest {
+
+    @Autowired
+    private BookRepository repository;
+
+    @Test
+    public void findByTitleShouldReturnBook() {
+        List<Book> books = repository.findByTitle("If tomorrow comes");
+        
+        assertThat(books).hasSize(1);
+        assertThat(books.get(0).getAuthor()).isEqualTo("Sydney Sheldon");
+    }
+    
+    @Test
+    public void createNewBook() {
+    	Book book = new Book("To Kill a Mockingbird ","Harper Lee", 1960 ,new Category("Thriller"),12293, 39.50);;
+    	repository.save(book);
+    	assertThat(book.getId()).isNotNull();
+    }    
+
+}
